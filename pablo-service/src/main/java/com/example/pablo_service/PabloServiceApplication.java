f5b75a25df12bc541126f45887b556fcc6e56317
Pablo Isaac <123191446+Rokablo@users.noreply.github.com>
2024-08-03 16:31:12 -0600

    Initial commit

---
+++ pablo-service/src/main/java/com/example/pablo_service/PabloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pablo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PabloServiceApplication.class, args);
+	}
+
+}
---
+++ pablo-service/src/test/java/com/example/pablo_service/PabloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pablo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
